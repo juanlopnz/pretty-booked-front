b948748b6a8fbf47aef443f8c8f6861faa586e1a
pablocardona03 <pabloccardona@gmail.com>
2025-08-15 16:12:31 -0500

    feat: add form view and input component with routing

---
+++ src/components/base/InputComponent.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+const props = defineProps<
+  {
+    label: string;
+    value: string;
+    placeholder?: string;
+    type?: string;
+    required?: boolean;
+  }
+>()
+
+const emit = defineEmits(['update:value'],)
+const onInput = (e: Event) => {
+  const target = e.target as HTMLInputElement
+  emit('update:value', target.value)
+}
+</script>
+
+<template>
+  <div class="mb-5">
+    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ props.label }}</label>
+    <input @input="onInput" :value="props.value" :type="props.type" :placeholder="props.placeholder"
+      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      :required="props.required" />
+  </div>
+</template>
---
+++ src/views/FormView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import InputComponent from '@/components/base/InputComponent.vue'
+import { ref } from 'vue'
+
+const formData = ref({
+  name: '',
+  email: '',
+  password: '',
+})
+const displayFormData = () => { console.log(formData.value)}
+
+const inputs = [
+  { label: 'Name', value: formData.value.name, placeholder:'', type: 'text'},
+  { label: 'Email', value: formData.value.email, placeholder:'', type: 'email'},
+  { label: 'Password', value: formData.value.password, placeholder:'', type: 'password'},
+]
+
+</script>
+
+
+<template>
+  <div>
+    <InputComponent v-for="(input, i) in inputs" :key="i" :label="input.label" v-model:value="input.value" :placeholder="input.placeholder" :type="input.type" />
+    <button @click="displayFormData" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">submit</button>
+  </div>
+</template>
