1c071259ada7658e38bbae02424dc22f329524c1
pablocardona03 <pabloccardona@gmail.com>
2025-08-17 19:11:10 -0500

    feat: refactor FormView to use dynamic input rendering and improve type safety

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -3,22 +3,23 @@ import InputComponent from '@/components/base/InputComponent.vue'
 import ButtonComponent from '@/components/base/ButtonComponent.vue'
 import { ref } from 'vue'
 
-const formData = ref({
+type FormKeys = 'name' | 'email' | 'password'
+const formData = ref<Record<FormKeys, string>>({
   name: '',
   email: '',
   password: '',
 })
-const displayFormData = () => { console.log(formData.value) }
-
+const inputs: { label: string; key: FormKeys; type: string; placeholder?: string; required?: boolean; }[] = [
+  { label: 'Name', key: 'name', type: 'text', required: true },
+  { label: 'Email', key: 'email', placeholder: 'example@example.com', type: 'email', required: true },
+  { label: 'Password', key: 'password', type: 'password' },
+]
 </script>
 
-
 <template>
-  <div class="flex flex-col gap-3 mt-10 justify-center w-100">
-    <InputComponent label="Name" v-model:value="formData.name" type="text" />
-    <InputComponent label="Email" v-model:value="formData.email" type="email" placeholder="example@example.com"/>
-    <InputComponent label="Password" v-model:value="formData.password" type="password" />
-    <ButtonComponent :action="displayFormData" name="Submit" />
-  </div>
-
+  <form class="flex flex-col gap-3 mt-10 justify-center w-100">
+    <InputComponent v-for="(input, i) in inputs" :key="i" :label="input.label" v-model:value="formData[input.key]"
+      :placeholder="input.placeholder" :type="input.type" :required="input.required" />
+    <ButtonComponent :action="() => console.log('Format error:', JSON.parse(JSON.stringify(formData)))" name="Submit" />
+  </form>
 </template>
